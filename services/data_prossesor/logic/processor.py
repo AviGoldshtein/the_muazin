28feafd7b030f1ed9086f083139c5c9f7b3c2b36
avi <a0548589558@gmail.com>
2025-09-07 16:14:21 +0300

    the unique id is generated

--- services/data_prossesor/logic/processor.py
+++ services/data_prossesor/logic/processor.py
@@ -1,6 +1,7 @@
-
+import hashlib
 
 
 class Processor:
-    def generate_id(self, file_name: str) -> str:
-        return ""
+    @staticmethod
+    def generate_id(file_name) -> str:
+        return hashlib.sha256(file_name.encode('utf-8')).hexdigest()
