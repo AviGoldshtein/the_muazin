ca65f663a5e27c1996e8dffedbf89ab017818c9b
avi <a0548589558@gmail.com>
2025-09-09 15:17:45 +0300

    fixed a bug in the consumer to not consume too much at a time, and wait a little more until the process is finished

--- services/speech_to_text/logic/consumer.py
+++ services/speech_to_text/logic/consumer.py
@@ -10,7 +10,11 @@ class Consumer:
         bootstrap_server = conf.KAFKA_BOOTSTRAP
         group_id = conf.CONSUMING_GROUP_ID_SPEECH_TO_TEXT
         return KafkaConsumer(topic,
-                                         group_id=group_id,
-                                         value_deserializer=lambda m: json.loads(m.decode('ascii')),
-                                         bootstrap_servers=[bootstrap_server]
-                                      )
+             group_id=group_id,
+             value_deserializer=lambda m: json.loads(m.decode('ascii')),
+             bootstrap_servers=[bootstrap_server],
+             max_poll_records=1,
+             max_poll_interval_ms= 30 * 60 * 1000,
+             session_timeout_ms= 3 * 60 * 1000,
+             auto_offset_reset= "earliest"
+          )
